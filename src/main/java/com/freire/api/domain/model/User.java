1857497fe4f0f2096f45ee24ebe0f2f5fb55e55b
levifreire7 <levifreire7@gmail.com>
2024-10-31 11:18:39 -0300

    Implementação da camada de dominio

---
+++ src/main/java/com/freire/api/domain/model/User.java
@@ -0,0 +1,35 @@
+package com.freire.api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<News> news;
+}
---
+++ src/main/java/com/freire/api/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.freire.api.domain.repository;
+
+import com.freire.api.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
